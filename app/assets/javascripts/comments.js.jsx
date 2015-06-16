9ad0dcbd364ce5f0be2308c6c7199ed8dc6381dd
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-06-16 15:46:56 -0400

    Got rails react comments working

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -1,55 +1,121 @@
-var CommentList = React.createClass({
-	render: function() {
-		return (
-			<div className="comment-list">
-				<Comment />
-			</div>
-		);
-	}
-});
+$(document).on('ready', function(){
 
 var Comment = React.createClass({
 	render: function() {
+
 		return (
 			<div className="comment">
-				<h2>Test Author</h2>
-				<p>lorem ipusem lorem isdfs sdf sd fjlsdf sd</p>
+			<h2 className="commentAuthor">
+			{this.props.author}
+			</h2>
+			<span>{this.props.text}</span>
 			</div>
-		);
-	}
-});
-
-var CommentForm = React.createClass({
-	render: function() {
-		return (
-			<form>
-				<input type="text" placeholder="Your name" />
-				<input type="text" placeholder="Say something..." />
-				<input type="submit" value="Submit" />
-			</form>
-		);
+			);
 	}
 });
 
 var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleCommentSubmit: function(comment) {
+		var comments = this.state.data;
+		comments.push(comment);
+		this.setState({data: comments}, function() {
+      // `setState` accepts a callback. To avoid (improbable) race condition,
+      // `we'll send the ajax request right after we optimistically set the new
+      // `state.
+      $.ajax({
+      	url: this.props.url,
+      	dataType: 'json',
+      	type: 'POST',
+      	beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      	data: {comment: comment},
+      	success: function(data) {
+      		this.setState({data: data});
+      	}.bind(this),
+      	error: function(xhr, status, err) {
+      		console.error(this.props.url, status, err.toString());
+      	}.bind(this)
+      });
+  });
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
-			<div className="comment-box">
-				<CommentForm />
-				<CommentList />
+			<div className="commentBox">
+			<h1>Comments</h1>
+			<CommentList data={this.state.data} />
+			<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
-		);
+			);
 	}
 });
 
-comments = 
-[
-    {
-        "author": "Pete Hunt",
-        "text": "Hey there!"
-    }
-]
+var CommentList = React.createClass({
+	render: function() {
+		var commentNodes = this.props.data.map(function(comment, index) {
+			return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        <Comment author={comment.author} text={comment.text} key={index} />
+        );
+		});
+		return (
+			<div className="commentList">
+			{commentNodes}
+			</div>
+			);
+	}
+});
 
-$(function() {
-	React.render(<CommentBox />, document.getElementById('content'));
-})
+var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = React.findDOMNode(this.refs.author).value.trim();
+		var text = React.findDOMNode(this.refs.text).value.trim();
+		if (!text || !author) {
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		React.findDOMNode(this.refs.author).value = '';
+		React.findDOMNode(this.refs.text).value = '';
+	},
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+			<input type="text" placeholder="Your name" ref="author" />
+			<input type="text" placeholder="Say something..." ref="text" />
+			<input type="submit" value="Post" />
+			</form>
+			);
+	}
+});
+
+
+var $content = $("#content");
+if ($content.length > 0) {
+	React.render(
+		<CommentBox url="comments.json" pollInterval={2000} />,
+		document.getElementById('content')
+		);
+}
+
+});
